1a21fbc3e31722669054861d2ce517e7324a6154
ben-whitten <benjamin.whitten@mths.ca>
2019-09-14 16:32:26 +0000

    added program

---
+++ rectangle.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2019 St. Mother Teressa HS All rights reserved.
+//
+// Created by: Ben Whitten
+// Created on: September 2019
+// This is a program which can find the area and perimeter or a rectangle.
+
+#include <iostream>
+#include <cmath>
+
+// This is what finds the area and perimeter of the rectangle.
+// Replace the two numbers at the end of lines 12, 14 and 15 to continue.
+int main() {
+    std::cout << "If a rectangle has the demensions:" << std::endl;
+    std::cout << "5cm x 3cm" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Then the area is" << (5*3) << "cm^2" << std::endl;
+    std::cout << "and the perimeter is" << (2*(5+3)) << "cm" << std::endl;
+}
